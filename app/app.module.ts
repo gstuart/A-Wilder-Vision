6658b4825226c173642786b8fccb5251860614ad
Grace Stuart <gstuart@gmail.com>
2017-05-05 16:48:02 -0700

    add animal-list and animalList

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule }          from '@angular/core';
 import { BrowserModule }     from '@angular/platform-browser';
 import { AppComponent }      from './app.component';
 import { FormsModule }       from '@angular/forms';
-import { AnimalComponent } from './animal.component';
+import { AnimalListComponent } from './animal-list.component';
 import { AnimalEditComponent } from './edit-animal.component';
 import { NewAnimalComponent }  from './new-animal.component';
 import { AgePipe }             from './age.pipe';
