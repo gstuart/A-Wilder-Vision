bd7427454a35f785aec8384084cc48d69787b448
Grace Stuart <gstuart@gmail.com>
2017-05-05 16:27:11 -0700

    add closing brace

--- app/age.pipe.ts
+++ app/age.pipe.ts
@@ -9,21 +9,22 @@ import {Animal} from './animal.model';
 export class AgePipe implements PipeTransform {
   transform(input: Animal[], animal) {
     var output: Animal[] = [];
-      if(animal === "young"){
-        for (var i = 0; i < input.length; i++) {
-          if (input[i].animal <= 2) {
-            output.push(input[i]);
-          }
+    if(animal === "young"){
+      for (var i = 0; i < input.length; i++) {
+        if (input[i].animal <= 2) {
+          output.push(input[i]);
         }
-        return output;
-      } else if (animal === "old"){
-        for (var i = 0; i < input.length; i++) {
-          if (input[i].animal >= 3) {
-            output.push(input[i]);
-          }
+      }
+      return output;
+    } else if (animal === "old"){
+      for (var i = 0; i < input.length; i++) {
+        if (input[i].animal >= 3) {
+          output.push(input[i]);
         }
-        return output;
-      }else {
-        return input;
+      }
+      return output;
+    }else {
+      return input;
+    }
   }
 }
