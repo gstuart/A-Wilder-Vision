a89e16d3b585906479501d6da8a363f74532725c
Grace Stuart <gstuart@gmail.com>
2017-05-05 14:58:06 -0700

    add animal module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,16 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }   from './app.component';
+import { NgModule }          from '@angular/core';
+import { BrowserModule }     from '@angular/platform-browser';
+import { AppComponent }      from './app.component';
+import { FormsModule }       from '@angular/forms';
+import { AnimalComponent } from './animal.component';
+import { AnimalEditComponent } from './edit-animal.component';
+import { NewAnimalComponent }  from './new-animal.component';
+import { AgePipe }             from './age.pipe';
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [ AppComponent ],
+  imports: [ BrowserModule,
+                  FormsModule ],
+  declarations: [ AppComponent, AnimalComponent, AnimalEditComponent, NewAnimalComponent ],
   bootstrap:    [ AppComponent ]
 })
 
