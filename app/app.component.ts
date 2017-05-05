013998f9e0046fc2e088b0d71e8db205500eecfd
Grace Stuart <gstuart@gmail.com>
2017-05-05 14:59:16 -0700

    add list of animals and sorting based on age

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
+import { Animal } from './animal.model';
 
 @Component({
   selector: 'app-root',
   template: `
     <div class="container">
       <h1>{{currentFocus}}</h1>
+      <p>
+        <animal-list [childAnimalList]="masterAnimalList"(clickSender)="editAnimal($event)"></animal-list>
+      </p>
+      <edit-animal [childSelectedAnimal]="selectedAnimal" (doneButtonClickedSender)="finishedEditing()">
+      </edit-animal>
+      <new-animal (newAnimalSender)="addAnimal($event)"></new-animal>
     </div>
   `
 })
 
 export class AppComponent {
   currentFocus: String = 'A Wilder Vision';
+
+  masterAnimalList: Animal[] = [
+    new Animal('Arctic Fox', 'Moon', 2, 'Carnivore', 'Northern Trail', 5, 'Female', 'Cool shade', 'Loud noises'),
+    new Animal('Ocelot', 'Prince', 4, 'Carnivore', 'Tropical Rain Forest Building', 6, 'Male', 'Laying in the sunshine', 'Toys that are not rope-based'),
+    new Animal('Northwest Black Tailed Deer', 'Tinkerbell', 8, 'Herbivore', 'Northern Trail', 2, 'Female', 'Delicate roots and leaves', 'Loud Noises')
+  ];
+
+  addAnimal(newAnimalFromChild: Animal) {
+    this.masterAnimalList.push(newAnimalFromChild);
+  }
+
+  editAnimal(clickedAnimal) {
+    this.selectedAnimal = clickedAnimal;
+  }
+
+  finishedEditing() {
+    this.selectedAnimal = null;
+  }
 }
