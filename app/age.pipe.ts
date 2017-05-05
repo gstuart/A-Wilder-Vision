e6c46f20e5b6e83d174b2ff9fa3fb9e57c80aa65
Grace Stuart <gstuart@gmail.com>
2017-05-05 16:25:36 -0700

    add pipe for sorting animals by age

---
+++ app/age.pipe.ts
@@ -0,0 +1,29 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import {Animal} from './animal.model';
+
+@Pipe ({
+  name: "age",
+  pure: false
+})
+
+export class AgePipe implements PipeTransform {
+  transform(input: Animal[], animal) {
+    var output: Animal[] = [];
+      if(animal === "young"){
+        for (var i = 0; i < input.length; i++) {
+          if (input[i].animal <= 2) {
+            output.push(input[i]);
+          }
+        }
+        return output;
+      } else if (animal === "old"){
+        for (var i = 0; i < input.length; i++) {
+          if (input[i].animal >= 3) {
+            output.push(input[i]);
+          }
+        }
+        return output;
+      }else {
+        return input;
+  }
+}
