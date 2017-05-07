8f41962401b27ff40533c6a254799320f39af0ec
Grace Stuart <gstuart@gmail.com>
2017-05-07 08:28:11 -0700

    add name to input

--- app/edit-animal.component.ts
+++ app/edit-animal.component.ts
@@ -9,13 +9,13 @@ import { Animal } from './animal.model';
       <fieldset *ngIf="childSelectedAnimal">
           <legend>Edit Animal</legend>
           <label for="species">Species</label>
-            <input [(ngModel)]="childSelectedAnimal.species" id="species" type="text">
+            <input [(ngModel)]="childSelectedAnimal.species" name="spiecies" id="species" type="text">
           <label for="name">Name</label>
-            <input [(ngModel)]="childSelectedAnimal.name" id="name" type="text">
+            <input [(ngModel)]="childSelectedAnimal.name" name="name" id="name" type="text">
           <label for="age">Age</label>
-            <input [(ngModel)]="childSelectedAnimal.age" id="age" type="text">
+            <input [(ngModel)]="childSelectedAnimal.age" name="age" id="age" type="text">
           <label for="diet">Diet</label>
-            <select [(ngModel)]="childSelectedAnimal.diet" id="diet">
+            <select [(ngModel)]="childSelectedAnimal.diet" name="diet" id="diet">
                 <option>Carnivor</option>
                 <option>Detritivore</option>
                 <option>Geophagy</option>
@@ -25,7 +25,7 @@ import { Animal } from './animal.model';
                 <option>Scavenger</option>
             </select>
           <label for="location">Location</label>
-            <select [(ngModel)]="childSelectedAnimal.location" id="location">
+            <select [(ngModel)]="childSelectedAnimal.location" name="location" id="location">
                 <option>Africa Plains</option>
                 <option>Africa Rocks</option>
                 <option>Asian Jungle</option>
@@ -37,7 +37,7 @@ import { Animal } from './animal.model';
                 <option>Outback</option>
             </select>
           <label for="caretakers">Caretakers</label>
-            <select [(ngModel)]="childSelectedAnimal.caretakers" id="caretakers">
+            <select [(ngModel)]="childSelectedAnimal.caretakers" name="caretakers" id="caretakers">
                 <option>1</option>
                 <option>2</option>
                 <option>3</option>
@@ -45,15 +45,15 @@ import { Animal } from './animal.model';
                 <option>5</option>
             </select>
           <label for="gender">Gender</label>
-            <select [(ngModel)]="childSelectedAnimal.gender" id="gender">
+            <select [(ngModel)]="childSelectedAnimal.gender" name="gender" id="gender">
                 <option>Male</option>
                 <option>Female</option>
                 <option>Unknown</option>
             </select>
           <label for="likes">Likes</label>
-            <input [(ngModel)]="childSelectedAnimal.likes" id="likes" type="text">
+            <input [(ngModel)]="childSelectedAnimal.likes" name="likes" id="likes" type="text">
           <label for="dislikes">Dislikes</label>
-            <input [(ngModel)]="childSelectedAnimal.dislikes" id="dislikes" type="text">
+            <input [(ngModel)]="childSelectedAnimal.dislikes" name="dislikes" id="dislikes" type="text">
           <button type="submit" class="pure-button pure-button-primary" (click)="doneButtonClicked()">Submit</button>
         </fieldset>
       </form>
